2b2af4a058aa770135f2daa47aaeb079962661ad
TheBlackTrust <officialblacktrust@gmail.com>
2023-09-09 21:42:57 +0100

    backend development

--- account/utils.py
+++ account/utils.py
@@ -28,7 +28,6 @@ def send_password_reset_verification_email(email, uid, token):
         f"Welcome to blacktrust.\n\n"
         f"You have requested for a password reset.\n"
         f"If this is not you, please ignore the next process and contact blacktrust administrative.\n"
-        # f"Please click the following link to verify your email: https://blacktrust.pythonanywhere.com/api/v1/account/user/email-verification/{uid}/{token}/"
         f"Please click the following link to verify your email: {settings.FRONTEND_BASE_URL}/login/{uid}/{token}/"
 
     )
